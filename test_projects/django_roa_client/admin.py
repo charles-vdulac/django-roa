d1887d2aee37c66b92e9c699054921a1d25411fa
David Larlet <larlet@gmail.com>
2009-01-08 11:14:29 +0100

    Add the CustomSlug example to the built-in admin

--- test_projects/django_roa_client/admin.py
+++ test_projects/django_roa_client/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_roa import ModelAdmin
-from models import RemotePage, RemotePageWithManyFields
+from models import RemotePage, RemotePageWithManyFields, RemotePageWithCustomSlug
 
 class RemotePageAdmin(ModelAdmin):
     list_per_page = 2
@@ -10,5 +10,10 @@ class RemotePageWithManyFieldsAdmin(ModelAdmin):
     list_display = ('__unicode__', 'boolean_field', 'char_field')
     list_filter = ('boolean_field',)
 
+
+class RemotePageWithCustomSlugAdmin(ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(RemotePage, RemotePageAdmin)
 admin.site.register(RemotePageWithManyFields, RemotePageWithManyFieldsAdmin)
+admin.site.register(RemotePageWithCustomSlug, RemotePageWithCustomSlugAdmin)
