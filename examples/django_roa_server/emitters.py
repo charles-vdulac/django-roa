8856729ecf12401b22733937e9158f7cdf08f454
David Larlet <larlet@gmail.com>
2009-05-10 17:24:22 +0200

    Use DjangoEmitter from piston's tip, adapted for logging

--- examples/django_roa_server/emitters.py
+++ examples/django_roa_server/emitters.py
@@ -4,36 +4,34 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.utils import simplejson
 
-from piston.emitters import Emitter, XMLEmitter
-from piston.utils import Mimer
+from piston.emitters import Emitter, DjangoEmitter
 
 logger = logging.getLogger("django_roa_server log")
 
-class DjangoEmitter(Emitter):
-    """
-    Emitter for the Django serialized format.
-    """
-    def serialize(self, format):
-        if isinstance(self.data, HttpResponse):
-            return self.data
-        elif isinstance(self.data, (int, str)):
-            response = self.data
-        else:
-            response = serializers.serialize(format, self.data, indent=True)
 
-        return response
-        
+class ROADjangoEmitter(DjangoEmitter):
+    """
+    ROA Django XML emitter.
+    
+    Just log the response with logging module.
+    """
     def render(self, request):
-        return self.serialize(format='xml')
-        
-Emitter.register('django', DjangoEmitter, 'application/xml; charset=utf-8')
+        response = super(ROADjangoEmitter, self).render(request, 'xml')
+        logger.debug(u"Response:\n%s" % response)
+        return response
+    
+Emitter.register('django', ROADjangoEmitter, 'application/xml; charset=utf-8')
 
 class CustomDjangoEmitter(DjangoEmitter):
     """
     Custom Django XML emitter.
+    
+    Use a custom serializer.
     """
     def render(self, request):
-        return self.serialize(format='custom')
+        response = super(CustomDjangoEmitter, self).render(request, 'custom')
+        logger.debug(u"Response:\n%s" % response)
+        return response
     
 Emitter.register('custom', CustomDjangoEmitter, 'application/xml; charset=utf-8')
 
